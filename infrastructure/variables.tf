5b461ab46fb58ff19632304a6bfcf6fc5fd38080
Krzysztof Knapik <krzysztof.knapik@nordcloud.com>
2024-02-24 20:32:30 +0100

    key

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -4,11 +4,12 @@ variable "resource_group_location" {
   description = "Location of the resource group."
 }
 
-variable "resource_group_name_prefix" {
+variable "resource_group_name" {
   type        = string
-  default     = "rg"
+  default     = "rg_vm_linux"
   description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
+
 
 variable "username" {
   type        = string
