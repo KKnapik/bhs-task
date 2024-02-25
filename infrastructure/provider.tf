7b9b84891e8cf38e1413813f5b68571e6efc748e
Krzysztof Knapik <krzysztof.knapik@nordcloud.com>
2024-02-25 01:29:47 +0100

    full

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -1,5 +1,5 @@
 terraform {
-#   required_version = ">= 1.5.7"
+  #   required_version = ">= 1.5.7"
   backend "azurerm" {
     resource_group_name  = "fstates"
     storage_account_name = "bhstskstf"
